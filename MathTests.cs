be3f2c24f4825330931c2d9852c41dfda0ce2041
Amatzu <dmitrysjestov@niuitmo.ru>
2018-02-21 21:46:46 +0300

    Added requierd math functions with descriptrion and tests

---
+++ MathTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace sdproject
+{
+	internal class MathTests
+	{
+		[Test, TestCase(5, 2, 10), TestCase(5, 3, 10)]
+		public void MinorCountTest(int matrixSize, int minorSize, int expectedResult)
+		{
+			Assert.AreEqual(expectedResult, MathUtils.MaxMinorAmount(matrixSize, minorSize));
+		}
+	}
+}
---
+++ MathUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdproject
+{
+	internal class MathUtils
+	{
+		/// <summary>
+		/// Максимальное количество миноров размера minorSize в квадратной матрице размера matrixSize.
+		/// </summary>
+		/// <param name="matrixSize"></param>
+		/// <param name="minorSize"></param>
+		/// <returns></returns>
+		public static int MaxMinorAmount(int matrixSize, int minorSize)
+		{
+			Debug.Assert(matrixSize > 0);
+			Debug.Assert(minorSize > 0);
+			Debug.Assert(matrixSize >= minorSize);
+			return simplexNumber(matrixSize - minorSize + 1, minorSize);
+		}
+
+		/// <summary>
+		/// N-е симплексное число порядка r.
+		/// </summary>
+		/// <param name="n">Номер симплексного числа</param>
+		/// <param name="r">Порядок симплексного числа</param>
+		/// <returns></returns>
+		private static int simplexNumber(int n, int r)
+		{
+			Debug.Assert(n >= 0);
+			Debug.Assert(r > 0);
+			int product = n;
+			for (int i = 2; i <= r; i++)
+			{
+				product *= n + i - 1;
+				product /= i;
+			}
+
+			return product;
+		}
+	}
+}
